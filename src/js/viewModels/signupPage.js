8880b3be008384974c29e1a818205c70e2d9d93b
thevenine <seyeoyeniran2u@gmail.com>
2019-09-18 16:49:20 +0100

    minor ui tweak

--- src/js/viewModels/signupPage.js
+++ src/js/viewModels/signupPage.js
@@ -6,14 +6,14 @@
  * Your dashboard ViewModel code goes here
  */
 define(['ojs/ojcore', 'knockout', 'jquery', 'ojs/ojselectcombobox', 'ojs/ojinputtext', 'ojs/ojbutton'],
-    function (oj, ko, $) {
+    function(oj, ko, $) {
 
         function SignupPageViewModel() {
             var self = this;
 
-            self.firstName = ko.observable();
-            self.middleName = ko.observable();
-            self.lastName = ko.observable();
+            self.firstname = ko.observable();
+            self.middlename = ko.observable();
+            self.lastname = ko.observable();
             self.email = ko.observable();
             self.username = ko.observable();
             self.phone = ko.observable();
@@ -21,7 +21,7 @@ define(['ojs/ojcore', 'knockout', 'jquery', 'ojs/ojselectcombobox', 'ojs/ojinput
             self.location = ko.observable();
             self.password = ko.observable();
             self.verifyPassword = ko.observable();
-            self.submitInfo = function () {
+            self.submitInfo = function() {
                 console.log("Button clicked..")
 
             };
@@ -32,7 +32,7 @@ define(['ojs/ojcore', 'knockout', 'jquery', 'ojs/ojselectcombobox', 'ojs/ojinput
                 "username": "janedoe",
                 "password": "jane"
             }
-            $.getJSON("https://volunteerng.herokuapp.com/api/login.php", data, function (res, textState, jqXHR) {
+            $.getJSON("https://volunteerng.herokuapp.com/api/login.php", data, function(res, textState, jqXHR) {
                 // oj.Router.rootInstance.go('loginPage');
 
 
@@ -58,7 +58,7 @@ define(['ojs/ojcore', 'knockout', 'jquery', 'ojs/ojselectcombobox', 'ojs/ojinput
             // );
 
 
-            self.loginClick = function () {
+            self.loginClick = function() {
 
                 oj.Router.rootInstance.go('loginPage');
 
@@ -78,7 +78,7 @@ define(['ojs/ojcore', 'knockout', 'jquery', 'ojs/ojselectcombobox', 'ojs/ojinput
              * @return {Promise|undefined} - If the callback returns a Promise, the next phase (attaching DOM) will be delayed until
              * the promise is resolved
              */
-            self.handleActivated = function (info) {
+            self.handleActivated = function(info) {
                 // Implement if needed
             };
 
@@ -91,7 +91,7 @@ define(['ojs/ojcore', 'knockout', 'jquery', 'ojs/ojselectcombobox', 'ojs/ojinput
              * @param {Function} info.valueAccessor - The binding's value accessor.
              * @param {boolean} info.fromCache - A boolean indicating whether the module was retrieved from cache.
              */
-            self.handleAttached = function (info) {
+            self.handleAttached = function(info) {
                 // Implement if needed
             };
 
@@ -104,7 +104,7 @@ define(['ojs/ojcore', 'knockout', 'jquery', 'ojs/ojselectcombobox', 'ojs/ojinput
              * @param {Node} info.element - DOM element or where the binding is attached. This may be a 'virtual' element (comment node).
              * @param {Function} info.valueAccessor - The binding's value accessor.
              */
-            self.handleBindingsApplied = function (info) {
+            self.handleBindingsApplied = function(info) {
                 // Implement if needed
             };
 
@@ -116,7 +116,7 @@ define(['ojs/ojcore', 'knockout', 'jquery', 'ojs/ojselectcombobox', 'ojs/ojinput
              * @param {Function} info.valueAccessor - The binding's value accessor.
              * @param {Array} info.cachedNodes - An Array containing cached nodes for the View if the cache is enabled.
              */
-            self.handleDetached = function (info) {
+            self.handleDetached = function(info) {
                 // Implement if needed
             };
         }
